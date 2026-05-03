0c77da43cfa3bc73de5f616635767e3e1d50b8f5
Priyanka <2501730192@krmu.edu.in>
2026-05-03 09:44:15 +0530

    Fixed Footer Position

--- Wanderer/src/components/Footer.jsx
+++ Wanderer/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="w-full py-4 mt-10 text-center text-sm bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-300">
+    <div className="fixed bottom-0 w-full py-4 mt-10 text-center text-sm bg-gray-100 dark:bg-gray-900 text-gray-700 dark:text-gray-300">
         <p className="text-sm text-center">&copy; 2026 Wanderer | All rights reserved | Made By Priyanka ❤️</p>
     </div>
   )
